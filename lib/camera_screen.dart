ee336a461b654052dfef1e560e739e197e1a1f17
Nitinjiwnani <jiwnani01@gmail.com>
2022-09-19 16:45:04 +0530

    error

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -14,35 +14,83 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  late CameraController controller;
+  late CameraController _controller;
+  late Future<void> cameraValue;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    controller =
+    _controller =
         new CameraController(widget.cameras[0], ResolutionPreset.medium);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
+    cameraValue = _controller.initialize();
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return new Container();
-    }
-    return new AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: new CameraPreview(controller),
+    return Scaffold(
+      body: Stack(children: [
+        FutureBuilder(
+            future: cameraValue,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(_controller);
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }),
+        Container(
+          color: Colors.black,
+          width: MediaQuery.of(context).size.width,
+          child: Column(children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.flash_off,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  onPressed: () {},
+                ),
+                InkWell(
+                  onTap: (() {}),
+                  child: Icon(
+                    Icons.panorama_fish_eye,
+                    color: Colors.white,
+                    size: 70,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.flip_camera_ios,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              "Hold for video, tap for photo",
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            )
+          ]),
+        ),
+      ]),
     );
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,77 +1,26 @@
 import 'dart:io';
 import 'package:camera_platform_interface/src/types/camera_description.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_image_picker/camera_screen.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
-  var cameras;
-  HomePage(this.cameras);
+  late List<CameraDescription> cameras;
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final picker = ImagePicker();
-  File? file;
-  XFile? pickedImage;
-  bool isLoading = false;
-  List<File?> fileList = [];
-
-  Future pickImageFromGallery(cameras) async {
-    pickedImage = await picker.pickImage(
-        source: ImageSource.camera, preferredCameraDevice: CameraDevice.rear);
-    setState(() {
-      file = File(pickedImage!.path);
-      fileList.add(file);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Image Upload"), actions: [
         IconButton(
             onPressed: () {
-              pickImageFromGallery(widget.cameras);
+              CameraScreen(widget.cameras);
             },
             icon: const Icon(Icons.image))
       ]),
-      body: GridView.builder(
-        itemCount: fileList.length,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (BuildContext context, int i) {
-          return Container(
-            padding: const EdgeInsets.all(10),
-            child: Stack(
-              children: <Widget>[
-                SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Image.file(
-                    File(fileList[i]!.path),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                    right: 1,
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          dltImages(fileList[i]);
-                        });
-                      },
-                      child: const Icon(Icons.cancel, color: Colors.red),
-                    ))
-              ],
-            ),
-          );
-        },
-      ),
     );
-  }
-
-  void dltImages(data) {
-    fileList.remove(data);
   }
 }
